13405c33a8dc5a1d338d6b88a3fa80d777f7b5cc
Vittorio Claudio Conte <v.conte@cnet>
2020-04-06 09:31:17 +0200

    Parte 1

---
+++ Parte 1/clothing.js
@@ -0,0 +1,11 @@
+function getClothing(isCold){   
+    
+    if (isCold) {
+       var freezing = 'Grab a jacket!';
+    } else {
+        var hot = 'It is a shorts kind of day!';
+        console.log(freezing);
+    }
+}
+
+getClothing(false);
